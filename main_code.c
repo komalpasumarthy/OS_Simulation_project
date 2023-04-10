ce055fda54c77b87481d4e9d2104fb01e0bfde89
komalpasumarthy <92156651+komalpasumarthy@users.noreply.github.com>
2023-04-10 23:33:44 +0530

    Update main_code.c

--- main_code.c
+++ main_code.c
@@ -1,112 +1,85 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-typedef struct {
-    int id;
+struct process {
+    int pid;
     int arrival_time;
-    int estimated_run_time;
+    int burst_time;
     int waiting_time;
-} Process;
+    int priority;
+};
+
+void print_gantt_chart(struct process proc[], int n);
 
 int main() {
-    int n, i, j, time = 0, total_waiting_time = 0;
-    float average_waiting_time;
-    Process *processes, temp;
-
-    // Step 1: Read the number of processes
+    int n, i, j, time=0, total_waiting_time=0;
+    float avg_waiting_time;
+    struct process temp;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
-
-    // Step 2: Allocate memory for the processes array
-    processes = (Process*) malloc(n * sizeof(Process));
-
-    // Step 3: Read the arrival time and estimated run time for each process
-    for (i = 0; i < n; i++) {
-        printf("Enter the arrival time for process %d: ", i+1);
-        scanf("%d", &processes[i].arrival_time);
-        printf("Enter the estimated run time for process %d: ", i+1);
-        scanf("%d", &processes[i].estimated_run_time);
-        processes[i].id = i+1;
-        processes[i].waiting_time = 0;
+    struct process proc[n];
+    for(i=0; i<n; i++) {
+        printf("Enter details for process %d:\n", i+1);
+        printf("Arrival time: ");
+        scanf("%d", &proc[i].arrival_time);
+        printf("Burst time: ");
+        scanf("%d", &proc[i].burst_time);
+        proc[i].pid = i+1;
+        proc[i].priority = 1 + proc[i].waiting_time/proc[i].burst_time;
     }
-
-    // Step 4: Sort the processes by arrival time
-    for (i = 0; i < n-1; i++) {
-        for (j = i+1; j < n; j++) {
-            if (processes[i].arrival_time > processes[j].arrival_time) {
-                temp = processes[i];
-                processes[i] = processes[j];
-                processes[j] = temp;
+    for(i=0; i<n-1; i++) {
+        for(j=0; j<n-i-1; j++) {
+            if(proc[j].priority > proc[j+1].priority) {
+                temp = proc[j];
+                proc[j] = proc[j+1];
+                proc[j+1] = temp;
             }
         }
     }
+    printf("Gantt chart:\n");
+    print_gantt_chart(proc, n);
+    printf("\nPID\tArrival Time\tBurst Time\tWaiting Time\n");
+    for(i=0; i<n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\n", proc[i].pid, proc[i].arrival_time, proc[i].burst_time, proc[i].waiting_time);
+        total_waiting_time += proc[i].waiting_time;
+    }
+    avg_waiting_time = (float)total_waiting_time/n;
+    printf("Average waiting time: %.2f\n", avg_waiting_time);
+    return 0;
+}
 
-    // Step 5: Run the processes using the priority scheduling algorithm
-    int *gantt_chart = (int*) malloc(n * 100 * sizeof(int));
-    int gantt_index = 0;
-    int *remaining_time = (int*) malloc(n * sizeof(int));
-    for (i = 0; i < n; i++) {
-        remaining_time[i] = processes[i].estimated_run_time;
+void print_gantt_chart(struct process proc[], int n) {
+    int i, j, current_time=0, completion_time;
+    printf(" ");
+    for(i=0; i<n; i++) {
+        for(j=0; j<proc[i].burst_time; j++) {
+            printf("--");
+        }
+        printf(" ");
     }
-    int *completed = (int*) malloc(n * sizeof(int));
-    for (i = 0; i < n; i++) {
-        completed[i] = 0;
+    printf("\n|");
+    for(i=0; i<n; i++) {
+        completion_time = current_time + proc[i].burst_time;
+        for(j=0; j<proc[i].burst_time-1; j++) {
+            printf(" ");
+        }
+        printf("P%d", proc[i].pid);
+        for(j=0; j<proc[i].burst_time-1; j++) {
+            printf(" ");
+        }
+        printf("|");
+        proc[i].waiting_time = current_time - proc[i].arrival_time;
+        if(proc[i].waiting_time < 0) {
+            proc[i].waiting_time = 0;
+        }
+        current_time = completion_time;
     }
-    int completed_count = 0;
-    while (completed_count < n) {
-        int highest_priority = -1;
-        int highest_priority_index = -1;
-        for (i = 0; i < n; i++) {
-            if (!completed[i] && processes[i].arrival_time <= time) {
-                int priority = 1 + processes[i].waiting_time / processes[i].estimated_run_time;
-                if (highest_priority == -1 || priority < highest_priority) {
-                    highest_priority = priority;
-                    highest_priority_index = i;
-                }
-            }
+    printf("\n ");
+    for(i=0; i<n; i++) {
+        for(j=0; j<proc[i].burst_time; j++) {
+            printf("--");
         }
-        if (highest_priority_index == -1) {
-            time++;
-            continue;
-        }
-        gantt_chart[gantt_index++] = highest_priority_index + 1;
-        remaining_time[highest_priority_index]--;
-        time++;
-        for (i = 0; i < n; i++) {
-            if (i != highest_priority_index && !completed[i] && processes[i].arrival_time <= time) {
-                processes[i].waiting_time++;
-            }
-        }
-        if (remaining_time[highest_priority_index] == 0) {
-            completed[highest_priority_index] = 1;
-            completed_count++;
-            processes[highest_priority_index].waiting_time = time - processes[highest_priority_index].arrival_time - processes[highest_priority_index].estimated_run_time;
-            total_waiting_time += processes[highest_priority_index].waiting_time;
-        }
-    }
-
-    // Step 6: Print the gantt chart
-    printf("\nGantt chart:\n");
-    for (i = 0; i < gantt_index; i++) {
-        printf("%d ", gantt_chart[i]);
+        printf(" ");
     }
     printf("\n");
-
-    // Step 7: Print the waiting time for each process
-    printf("\nWaiting time for each process:\n");
-    for (i = 0; i < n; i++) {
-        printf("Process %d: %d\n", processes[i].id, processes[i].waiting_time);
-    }
-
-    // Step 8: Calculate and print the average waiting time
-    average_waiting_time = (float) total_waiting_time / n;
-    printf("\nAverage waiting time: %.2f\n", average_waiting_time);
-
-    // Step 9: Free the memory allocated for the arrays
-    free(processes);
-    free(gantt_chart);
-    free(remaining_time);
-    free(completed);
-
-    return 0;
 }
